1f9ce5a4914da1fbdc49d036511fc7a3e378e0ad
Gryisic <leha.sysolyatin@yandex.ru>
2023-10-10 21:20:36 +0500

    Added fancy visual

---
+++ Assets/Scripts/Common/Models/Particles/UnitParticlesPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common.Models.Particles
+{
+    public class UnitParticlesPlayer
+    {
+        public void Play(ParticleSystem particleSystem)
+        {
+            particleSystem.Play();
+        }
+    }
+}
